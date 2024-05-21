b5f4aed9641c6e6c6ee8aadc0a2f209ec7f17a14
roman-ks <80638824+roman-ks@users.noreply.github.com>
2024-05-21 21:22:30 +0300

    caching-spring: add ehcache to cache results returned from hazelcast

--- caching-spring/src/main/java/com/mashkario/cache/service/HazelcastService.java
+++ caching-spring/src/main/java/com/mashkario/cache/service/HazelcastService.java
@@ -3,12 +3,18 @@ package com.mashkario.cache.service;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import com.mashkario.cache.dto.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
 @Component
 public class HazelcastService {
+
+    private static final Logger logger = LoggerFactory.getLogger(HazelcastService.class);
 
     public static final String MAP_NAME = "People";
 
@@ -18,12 +24,15 @@ public class HazelcastService {
         this.hazelcastInstance = hazelcastInstance;
     }
 
+    @CacheEvict(value = "personsCache", key = "#id")
     public Person put(String id, Person person) {
         IMap<String, Person> map = hazelcastInstance.getMap(MAP_NAME);
-        return map.putIfAbsent(id, person);
+        return map.put(id, person);
     }
 
+//    @Cacheable(value = "personsCache", key="#id")
     public Person get(String id) {
+        logger.info("Getting person {}", id);
         IMap<String, Person> map = hazelcastInstance.getMap(MAP_NAME);
         return map.get(id);
     }
