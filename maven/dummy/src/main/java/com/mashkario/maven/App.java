85b8b5bcf2b8acdb000ed900597815136cc97ddd
roman-ks <80638824+roman-ks@users.noreply.github.com>
2024-05-25 13:19:52 +0300

    maven: generate dummy project with archetype

---
+++ maven/dummy/src/main/java/com/mashkario/maven/App.java
@@ -0,0 +1,13 @@
+package com.mashkario.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! (from com.mashkario.maven:archetype)" );
+    }
+}
